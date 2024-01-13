b6fcb012223cd524e0cef5a1329f62e9deae3ca7
FennX1000 <fennwalkinshaw@outlook.com>
2024-01-13 15:18:01 +0000

    Fishing Rod Stand (BE) IN PROGRESS

--- src/main/java/fennx1000/chronicles/block/custom/entity/FishingRodStandEntity.java
+++ src/main/java/fennx1000/chronicles/block/custom/entity/FishingRodStandEntity.java
@@ -1,20 +1,65 @@
 package fennx1000.chronicles.block.custom.entity;
 
+import net.minecraft.block.BarrelBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BarrelBlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.block.entity.ViewerCountManager;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class FishingRodStandEntity extends LootableContainerBlockEntity {
-    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(2,ItemStack.EMPTY);
+    private DefaultedList<ItemStack> inventory = DefaultedList.ofSize(2,ItemStack.EMPTY);
 
-    private static final int SLOT1 = 0;
+    private final ViewerCountManager stateManager = new ViewerCountManager(){
+
+
+        @Override
+        protected void onContainerOpen(World world, BlockPos pos, BlockState state) {
+            world.playSoundAtBlockCenter(pos, SoundEvents.BLOCK_BARREL_OPEN, SoundCategory.BLOCKS, 10, 1, true);
+            FishingRodStandEntity.this.setOpen(state, true);
+        }
+
+        @Override
+        protected void onContainerClose(World world, BlockPos pos, BlockState state) {
+            world.playSoundAtBlockCenter(pos, SoundEvents.BLOCK_BARREL_OPEN, SoundCategory.BLOCKS, 10, 1, true);
+            FishingRodStandEntity.this.setOpen(state, false);
+        }
+
+        @Override
+        protected void onViewerCountUpdate(World world, BlockPos pos, BlockState state, int oldViewerCount, int newViewerCount) {
+        }
+
+        @Override
+        protected boolean isPlayerViewing(PlayerEntity player) {
+            if (player.currentScreenHandler instanceof GenericContainerScreenHandler) {
+                Inventory inventory = ((GenericContainerScreenHandler)player.currentScreenHandler).getInventory();
+                return inventory == FishingRodStandEntity.this;
+            }
+            return false;
+        }
+    };
+
+
+    void setOpen(BlockState state, boolean open) {
+
+        }
+
+        private static final int SLOT1 = 0;
     private static final int SLOT2 = 1;
     private static final int SLOT3 = 2;
     private static final int SLOT4 = 3;
@@ -42,6 +87,23 @@ public class FishingRodStandEntity extends LootableContainerBlockEntity {
     @Override
     protected ScreenHandler createScreenHandler(int syncId, PlayerInventory playerInventory) {
         return GenericContainerScreenHandler.createGeneric9x3(syncId, playerInventory, this);
+    }
+
+    @Override
+    protected void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        if (!this.serializeLootTable(nbt)) {
+            Inventories.writeNbt(nbt, this.inventory);
+        }
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        this.inventory = DefaultedList.ofSize(this.size(), ItemStack.EMPTY);
+        if (!this.deserializeLootTable(nbt)) {
+            Inventories.readNbt(nbt, this.inventory);
+        }
     }
 
     @Override
